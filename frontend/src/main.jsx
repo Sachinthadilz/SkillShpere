2d9877b7f6bb84fa301f3ebe5b423225afa59f8f
Sachintha Dilshan <sachinthadilshan17285@gmail.com>
2025-05-06 21:34:13 +0530

    main.jsx updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { ContextProvider } from "./store/ContextApi.jsx";
+import { Toaster } from "react-hot-toast";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ContextProvider>
+      <App />
+      <Toaster />
+    </ContextProvider>
+  </StrictMode>
+);
