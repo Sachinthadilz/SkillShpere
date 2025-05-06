b231367e24e8a69d78b08202fb4264b2383fe9f4
Sachintha Dilshan <sachinthadilshan17285@gmail.com>
2025-05-06 21:55:55 +0530

    add login,signup paths to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Footer from "./components/Footer/Footer";
 import ContactUs from "./components/ContactUs/ContactUs";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Landing from "./components/LandingPage/LandingPage";
+import Signup from "./components/Auth/SignUp";
+import Login from "./components/Auth/Login";
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
         <Route path="/" element={<Landing />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </Router>
