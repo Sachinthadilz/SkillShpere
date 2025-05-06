6550b19b0202a6a2a342cc58638a72aec34989cd
Sachintha Dilshan <sachinthadilshan17285@gmail.com>
2025-05-06 20:20:14 +0530

    add cloudinary config

--- SkillSphere/src/main/java/com/skillshare/platform/config/CloudinaryConfig.java
+++ SkillSphere/src/main/java/com/skillshare/platform/config/CloudinaryConfig.java
@@ -1,10 +1,8 @@
 package com.skillshare.platform.config;
-
 import com.cloudinary.Cloudinary;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.HashMap;
 import java.util.Map;
 
