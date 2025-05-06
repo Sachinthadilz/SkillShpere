213ba39b7bc9710801141e6bee5e06960acd0d7a
Sachintha Dilshan <sachinthadilshan17285@gmail.com>
2025-05-06 22:10:13 +0530

    add pathes to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,13 @@ import AboutUs from "./components/AboutUs/AboutUs";
 import Landing from "./components/LandingPage/LandingPage";
 import Signup from "./components/Auth/SignUp";
 import Login from "./components/Auth/Login";
+import OAuth2RedirectHandler from "./components/Auth/OAuth2RedirectHandler";
+import UserProfile from "./components/Auth/UserDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AccessDenied from "./components/Auth/AccessDenied";
 import NotFound from "./components/NotFound";
+import ForgotPassword from "./components/Auth/ForgotPassword";
+import ResetPassword from "./components/Auth/ResetPassword";
 
 const App = () => {
   return (
@@ -19,6 +25,18 @@ const App = () => {
         <Route path="/about" element={<AboutUs />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/access-denied" element={<AccessDenied />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <UserProfile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
