0ec1d961a66c1342000e9fdebb8de83036b4de85
osanda lakshitha <osandalakshitha01@gmail.com>
2025-05-07 22:05:02 +0530

    Added Commenting model

---
+++ SkillSphere/src/main/java/com/skillshare/platform/models/Comment.java
@@ -0,0 +1,15 @@
+package com.example.skillshare.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "comments")
+public class Comment {
+    @Id
+    private String id;
+    private String postId;
+    private String author;
+    private String content;
+}
