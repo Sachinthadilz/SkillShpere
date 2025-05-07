a7de8d921aa0450adb14aa9d9ce49ff17c342026
osanda lakshitha <osandalakshitha01@gmail.com>
2025-05-07 22:05:46 +0530

    Added Like model

---
+++ SkillSphere/src/main/java/com/skillshare/platform/models/Like.java
@@ -0,0 +1,14 @@
+package com.example.skillshare.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "likes")
+public class Like {
+    @Id
+    private String id;
+    private String postId;
+    private String username;
+}
